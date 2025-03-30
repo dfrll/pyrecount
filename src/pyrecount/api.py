5dc7ece61ac89d4a545ae98b116d19a23878875e
tn <db2025f@gmail.com>
2025-03-30 13:54:37 -0400

    refactor api class

--- src/pyrecount/api.py
+++ src/pyrecount/api.py
@@ -4,28 +4,27 @@ from os import path
 from typing import Optional, Dict
 from .models import HOMES_INDEX
 from requests import Response, get
+from dataclasses import dataclass, field
 from requests.exceptions import RequestException
 
 log = logging.getLogger()
 
 
+@dataclass
 class EndpointConnector:
-    def __init__(
-        self, organism: str, root_url: str = "http://duffel.rail.bio/recount3"
-    ):
-        self.organism: str = organism
-        self.root_url: str = root_url
+    organism: str
+    root_url: str = "http://duffel.rail.bio/recount3"
 
-        self.data_sources: Dict[str, str] = {}
+    root_organism_url: str = field(init=False)
+    data_sources: Dict[str, str] = field(init=False)
+
+    def __post_init__(self):
+        self.root_organism_url = path.join(self.root_url, self.organism)
 
         index = path.join(self.root_organism_url, HOMES_INDEX)
         resp = self._validate_endpoint(endpoint=index)
         if resp:
             self._set_data_sources(resp)
-
-    @property
-    def root_organism_url(self) -> str:
-        return path.join(self.root_url, self.organism)
 
     def _set_data_sources(self, resp: Response):
         self.data_sources: Dict[str, str] = {
