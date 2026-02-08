76f4bc867ce809b1bc0a8c30c96c9cf1acd3d386
tn <db2025f@gmail.com>
2026-02-08 08:30:45 -0500

    auc

--- src/pyrecount/accessor.py
+++ src/pyrecount/accessor.py
@@ -177,47 +177,9 @@ class Project:
             case Dtype.JXN:
                 return self._jxn_load()
             case Dtype.GENE:
-                # XXX:
-                # TODO: force sample name alignment before scaling
-
-                targetSize = 4e7
-                L = 100
-
-                md = self._metadata_load()
-                mapped_reads = md["star.all_mapped_reads"].cast(pl.Float64)
-                avg_mapped_read_length = md["star.average_mapped_length"].cast(
-                    pl.Float64
-                )
-                avg_read_length = md["avg_len"].cast(pl.Float64)
-
-                ratio = (avg_mapped_read_length / avg_read_length).round(0)
-                ratio = ratio.set(~ratio.is_in([1, 2]), None)
-
-                paired_end = ratio == 2
-                paired_factor = pl.when(paired_end).then(2).otherwise(1)
-
-                scaleFactor = md.select(
-                    (
-                        targetSize
-                        * L
-                        * paired_factor
-                        / (mapped_reads * avg_mapped_read_length**2)
-                    ).alias("scaleFactor")
-                ).to_series()
-
-                _, counts = self._gene_load()
-
-                numeric_cols = counts.select(pl.selectors.numeric()).columns
-                scaled = counts.with_columns(
-                    [
-                        (pl.col(c) * sf).round(0)
-                        for c, sf in zip(numeric_cols, scaleFactor)
-                    ]
-                )
-
-                print(scaled)
-
-                return pl.DataFrame(), pl.DataFrame()
+                annotation, counts = self._gene_load()
+                target_size = 4e7
+                return annotation, self._scale_auc(counts, target_size)
             case Dtype.EXON:
                 return self._exon_load()
             case Dtype.BW:
